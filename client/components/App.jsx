27f0c246375d6650437592e44f2586e98b3467e9
Jimmy Ko <ko.jimmy09@gmail.com>
2021-06-29 16:09:18 -0400

    initialized App.jsx as a functional component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-    }
-  }
-
-  render() {
-    return (
-      <div className="app">
-        Hello World
-      </div>
-    )
-  }
+export const App = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
 }
-
-export default App;
