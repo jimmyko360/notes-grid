c56f59f52f0da004c62e3ef6cac542cdeb19882d
Jimmy Ko <ko.jimmy09@gmail.com>
2021-06-29 21:53:35 -0400

    created methods to edit and delete notes

--- client/NotesProvider.jsx
+++ client/NotesProvider.jsx
@@ -3,7 +3,11 @@ import React, {useState, createContext} from 'react'
 export const NotesContext = createContext({});
 
 export const NotesProvider = ({children}) => {
-  const [notes, setNotes] = useState(['note1', 'note2', 'note3']);
+  let notesArray = [];
+  for (let i = 0; i < 36; i++) {
+    notesArray.push('New Note');
+  }
+  const [notes, setNotes] = useState(notesArray);
   const [selected, setSelected] = useState({
     first: false,
     second: false
@@ -12,7 +16,17 @@ export const NotesProvider = ({children}) => {
   return (
     <NotesContext.Provider value={{
       notes,
-      selected
+      selected,
+      editNote: (text, index) => {
+        let newNote = [...notes];
+        newNote[index] = text;
+        setNotes(newNote);
+      },
+      deleteNote: (index) => {
+        let newNote = [...notes];
+        newNote[index] = 'New Note';
+        setNotes(newNote);
+      }
     }}>
       {children}
     </NotesContext.Provider>
