c6fe258fd8d90aed67b3036531e15f0a251b04b9
Jimmy Ko <ko.jimmy09@gmail.com>
2021-06-30 00:17:59 -0400

    created note input form and rendered to page

---
+++ client/components/AddNote.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const AddNote = () => {
+  return (
+    <div>
+      <form>
+        <textarea/>
+        <input type='submit'/>
+      </form>
+    </div>
+  )
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,10 +2,12 @@ import React, {useContext} from 'react';
 import {NotesProvider} from '../NotesProvider.jsx';
 import {NotesContext} from '../NotesProvider.jsx';
 import {NoteGrid} from './NoteGrid.jsx';
+import {AddNote} from './AddNote';
 
 export const App = () => {
   return (
     <NotesProvider>
+      <AddNote/>
       <NoteGrid/>
     </NotesProvider>
   )
