08f51134f3dbaf2081057ec1639a88fa21ea10b0
Jimmy Ko <ko.jimmy09@gmail.com>
2021-06-30 01:16:07 -0400

    created a form to add a new note

--- client/components/AddNote.jsx
+++ client/components/AddNote.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {NotesContext} from '../NotesProvider'
 
 export const AddNote = () => {
+  const {notes, editNote} = useContext(NotesContext);
+
   return (
-    <div>
-      <form>
-        <textarea/>
-        <input type='submit'/>
+    <div className='submission'>
+      <form
+        onSubmit={(event)=>{
+          event.preventDefault();
+          editNote(event.target[0].value, notes.length);
+        }}
+      >
+        <textarea rows="4" cols="50"/>
+        <input type='submit' value='Add New Note'/>
       </form>
     </div>
   )
