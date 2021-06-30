0e3882ed13b67c289a3ce6d448e9cfca19d8a8a5
Jimmy Ko <ko.jimmy09@gmail.com>
2021-06-29 21:54:16 -0400

    created form to edit notes, passed methods to edit and delete notes

--- client/components/Note.jsx
+++ client/components/Note.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {NotesContext} from '../NotesProvider.jsx';
 
-export const Note = ({text}) => {
+export const Note = ({text, index}) => {
+  const [editing, setEditing] = useState(false);
+
+  const {editNote, deleteNote} = useContext(NotesContext);
+
   return (
     <div>
-      {text}
+      <button
+        onClick={()=>{setEditing(!editing)}}
+      >
+        {text === 'New Note' ? 'Create Note' : 'Edit Note'}
+      </button>
+      <button
+        onClick={()=>{deleteNote(index)}}
+      >
+        X
+      </button>
+      {editing ?
+        <form
+          onSubmit={(event)=>{
+            editNote(event.target[0].value, index);
+            setEditing(false);
+          }}
+        >
+          <input type='text' defaultValue={text}/>
+          <input type='submit' value={'Done'}/>
+        </form>
+      : text}
     </div>
   )
 }
